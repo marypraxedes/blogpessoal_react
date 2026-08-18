7bcf243e02100443d2f90cf33cc1c2d5aaa39266
Maryane Praxedes <mary.praxedes@icloud.com>
2026-08-18 09:00:03 -0300

    :sparkles: Criação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
 function App() {
 
 
   return (
     <>
+    <BrowserRouter >
       <Navbar />
-      <Home />
+      <div className= "min-h-[80vh]">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cadastro' element={<Cadastro />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      </div>
       <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router"
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
 
                 <div className="container flex justify-between text-lg mx-8">
-                    Blog Pessoal
+                    <Link to="/" className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens
                         Temas
                         Cadastrar tema
                         Perfil
-                        Sair
+                        <Link to="/login" className="hover:underline">Sair</Link>
                     </div>
                 </div>
 
