890877d92e749c7bfb0c1ec53874b1dd573ccee1
Maryane Praxedes <mary.praxedes@icloud.com>
2026-08-19 16:58:27 -0300

    :sparkles: Implementação do Cadastro e Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext);
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+
     return (
         <>
             <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
@@ -13,7 +27,7 @@ function Navbar() {
                         Temas
                         Cadastrar tema
                         Perfil
-                        <Link to="/login" className="hover:underline">Sair</Link>
+                        <Link to='' onClick={logout} className="hover:underline">Sair</Link>
                     </div>
                 </div>
 
--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,9 +1,177 @@
-
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import type Usuario from "../../models/Usuario";
+import { cadastrarUsuario } from "../../services/Service"; 
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function Cadastro() {
+
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [usuario, setUsuario] = useState<Usuario>({
+    id: 0,
+    nome: '',
+    usuario: '',
+    senha: '',
+    foto: '',
+  });
+
+  const [confirmarSenha, setConfirmarSenha] = useState<string>('');
+
+  useEffect(() => {
+    if (usuario.id !== 0) {
+     retornar();
+    }
+  }, [usuario.id]);
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value);
+  }
+
+  async function cadastrarNovoUsuario(e: FormEvent<HTMLFormElement>) { 
+    e.preventDefault();
+
+    if (confirmarSenha !== usuario.senha || usuario.senha.length < 8) {
+      alert("As senhas não coincidem e/ou não possuem pelo menos 8 caracteres.");
+      setUsuario({
+        ...usuario,
+        senha: '',
+      });
+      setConfirmarSenha('');
+      return;
+    }
+
+    setIsLoading(true); 
+
+    const usuarioParaEnviar = {
+      nome: usuario.nome,
+      usuario: usuario.usuario,
+      senha: usuario.senha,
+      foto: usuario.foto.trim() === '' ? 'https://i.imgur.com/39110eh.png' : usuario.foto
+    };
+
+    try {
+      await cadastrarUsuario(`/usuarios/cadastrar`, usuarioParaEnviar, setUsuario);
+      alert("Usuário cadastrado com sucesso!");
+      navigate("/login");
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        alert(`Erro ao cadastrar usuário: ${error.response.data.message || error.response.data}`);
+      } else {
+        alert("Erro ao cadastrar usuário, verifique a conexão com a API.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+function retornar() {
+    navigate("/login");
+  }
   return (
-    <div>Cadastro</div>
-  )
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold">
+        <div className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat w-full min-h-screen bg-cover bg-center"></div>
+        
+        <form className='flex justify-center items-center flex-col w-2/3 gap-3' onSubmit={cadastrarNovoUsuario}>
+          <h2 className='text-slate-900 text-5xl'>Cadastrar</h2>
+          
+          <div className="flex flex-col w-full">
+            <label htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              name="nome"
+              placeholder="Nome"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.nome}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label htmlFor="usuario">Usuario</label>
+            <input
+              type="text"
+              id="usuario"
+              name="usuario"
+              placeholder="Usuario"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label htmlFor="foto">Foto</label>
+            <input
+              type="text"
+              id="foto"
+              name="foto"
+              placeholder="Foto"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              name="senha"
+              placeholder="Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            />
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label htmlFor="confirmarSenha">Confirmar Senha</label>
+            <input
+              type="password"
+              id="confirmarSenha"
+              name="confirmarSenha"
+              placeholder="Confirmar Senha"
+              className="border-2 border-slate-700 rounded p-2"
+              value={confirmarSenha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleConfirmarSenha(e)}
+            />
+          </div>
+
+          <div className="flex justify-around w-full gap-8">
+            <button
+              type='button'
+              className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2'
+              onClick={retornar}
+            >
+              Cancelar
+            </button>
+            <button
+              type='submit'
+              className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2 flex justify-center items-center'
+            >
+              {isLoading ? (
+                <ClipLoader color="#ffffff" size={24} />
+              ) : (
+                <span>Cadastrar</span>
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Cadastro
+export default Cadastro;
